1cffe371ad57fa840a2909e1b1ba9ce77f25261a
bls <bls3427@outlook.com>
2019-04-10 19:36:19 -0700

    Redo storage

--- voxel.py
+++ voxel.py
@@ -24,33 +24,12 @@ import wave
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 
-sDEVINDEX    = 'DEVINDEX'
-sTHRESHOLD   = 'THRESHOLD'
-sSAVERECS    = 'SAVERECS'
-sHANGDELAY   = 'HANGDELAY'
-sCHUNK       = 'CHUNK'
-sDEVRATE     = 'DEVRATE'
-sCURRENT     = 'CURRENT'
-sRECORDFLAG  = 'RECORDFLAG'
-sRUNNING     = 'RUNNING'
-sPEAKFLAG    = 'PEAKFLAG'
-sTTYSETTINGS = 'TTYSETTINGS'
-sTTYFD       = 'TTYFD'
-sPREQUE      = 'PREQUE'
-sRCNT        = 'RCNT'
-sPYAUDIO     = 'PYAUDIO'
-sDEVSTREAM   = 'DEVSTREAM'
-sRT          = 'RT'
-sPROCESSOR   = 'PROCESSOR'
-sSAMPLEQUEUE = 'SAMPLEQUEUE'
-sKM          = 'KM'
-
-PDAT = {}
-PDAT[sDEVINDEX] = PDAT[sTHRESHOLD] = PDAT[sSAVERECS] = PDAT[sHANGDELAY] = PDAT[sCHUNK] = PDAT[sDEVRATE] = PDAT[sCURRENT] = 0
-PDAT[sRECORDFLAG] = PDAT[sRUNNING] = PDAT[sPEAKFLAG] = False
-PDAT[sTTYSETTINGS] = PDAT[sTTYFD] = PDAT[sPYAUDIO] = PDAT[sDEVSTREAM] =PDAT[sPROCESSOR] = PDAT[sSAMPLEQUEUE] = None
-PDAT[sPREQUE] = {}
-PDAT[sRCNT] = 0
+class voxdat:
+    def __init__(self):
+        self.devindex = self.threshold = self.saverecs = self.hangdelay = self.chunk = self.devrate = self.current = self.rcnt = 0
+        self.recordflag = self.running = self.peakflag = False
+        self.rt = self.km = self.ttysettings = self.ttyfd = self.pyaudio = self.devstream = self.processor = None
+        self.preque = self.samplequeue = None
 
 # Alsa error message blocking
 
@@ -75,38 +54,38 @@ class _streamProcessor(threading.Thread):
         threading.Thread.__init__(self)
         self.setDaemon(True)
         self.pdat = pdat
-        self.rt = self.pdat[sRT]
-        self.queue = self.pdat[sSAMPLEQUEUE]
-        self.prequeue = self.pdat[sPREQUE]
+        self.rt = self.pdat.rt
+#        self.queue = self.pdat.samplequeue
+#        self.prequeue = self.pdat.preque
         self.wf = None
         self.filename = "No File"
         
     def run(self):
-        while self.pdat[sRUNNING]:
-            data = self.queue.get(1)
+        while self.pdat.running:
+            data = self.pdat.samplequeue.get(1)
             if data == None:
                 time.sleep(0.1)
             else:
                 data2 = np.fromstring(data,dtype=np.int16)
                 peak = np.average(np.abs(data2))
                 peak = (100*peak)/2**12
-                self.pdat[sCURRENT] = int(peak)
-                if self.pdat[sCURRENT] > self.pdat[sTHRESHOLD]:
+                self.pdat.current = int(peak)
+                if self.pdat.current > self.pdat.threshold:
                     self.rt.reset_timer(time.time())
-                if self.pdat[sRECORDFLAG]:
+                if self.pdat.recordflag:
                     if not self.wf:
                         self.filename = time.strftime("%Y%m%d-%H%M%S.wav")
                         print("opening file " + self.filename + "\r")
                         self.wf = wave.open(self.filename, 'wb')
                         self.wf.setnchannels(CHANNELS)
-                        self.wf.setsampwidth(self.pdat[sPYAUDIO].get_sample_size(FORMAT))
-                        self.wf.setframerate(self.pdat[sDEVRATE])
-                        if self.pdat[sRCNT] != 0:
-                            self.pdat[sRCNT] = 0
+                        self.wf.setsampwidth(self.pdat.pyaudio.get_sample_size(FORMAT))
+                        self.wf.setframerate(self.pdat.devrate)
+                        if self.pdat.rcnt != 0:
+                            self.pdat.rcnt = 0
                             while True:
                                 try:
                                     data3 = None
-                                    data3 = self.prequeue.get_nowait()
+                                    data3 = self.pdat.preque.get_nowait()
                                     self.wf.writeframes(data3)
                                 except:
                                     pass
@@ -114,16 +93,16 @@ class _streamProcessor(threading.Thread):
                                 pass
                     self.wf.writeframes(data)
                 else:
-                    if self.pdat[sRCNT] == self.pdat[sSAVERECS]:
-                        data3 = self.prequeue.get_nowait()
+                    if self.pdat.rcnt == self.pdat.saverecs:
+                        data3 = self.pdat.preque.get_nowait()
                     else:
-                        self.pdat[sRCNT] =  self.pdat[sRCNT]+1
-                    self.prequeue.put(data)
+                        self.pdat.rcnt +=1  #  self.pdat.rcnt+1
+                    self.pdat.preque.put(data)
                     pass
              
     def ReadCallback(self, indata, framecount, timeinfo, status):
-        self.queue.put(indata)
-        if self.pdat[sRUNNING]:
+        self.pdat.samplequeue.put(indata)
+        if self.pdat.running:
             return(None, pyaudio.paContinue)
         else:
             return(None, pyaudio.paAbort)
@@ -142,19 +121,19 @@ class _recordTimer(threading.Thread):
         self.timer = 0
         
     def run(self):
-        while self.pdat[sRUNNING]:
-            if time.time() - self.timer < self.pdat[sHANGDELAY]:
-                self.pdat[sRECORDFLAG] = True
-            if time.time() - self.timer > self.pdat[sHANGDELAY] + 1:
-                self.pdat[sRECORDFLAG] = False
-                self.pdat[sPROCESSOR].close()
-            if self.pdat[sPEAKFLAG]:
-                nf = min (self.pdat[sCURRENT], 99)
+        while self.pdat.running:
+            if time.time() - self.timer < self.pdat.hangdelay:
+                self.pdat.recordflag = True
+            if time.time() - self.timer > self.pdat.hangdelay + 1:
+                self.pdat.recordflag = False
+                self.pdat.processor.close()
+            if self.pdat.peakflag:
+                nf = min (self.pdat.current, 99)
                 nf2 = nf
                 if nf > 50: nf = int(min(50 + (nf - 50)/3, 72))
                 if nf <= 0: nf=1
                 rf = ""
-                if self.pdat[sRECORDFLAG]: rf = "*"
+                if self.pdat.recordflag: rf = "*"
                 print("{} {}{}\r".format("#"*nf, nf2, rf))
             time.sleep(1)
                 
@@ -168,11 +147,11 @@ class KBListener(threading.Thread):
         self.setDaemon(True)
 
     def treset(self):
-        termios.tcsetattr(self.pdat[sTTYFD], termios.TCSADRAIN, self.pdat[sTTYSETTINGS])
+        termios.tcsetattr(self.pdat.ttyfd, termios.TCSADRAIN, self.pdat.ttysettings)
 
     def getch(self):
         try:
-            tty.setraw(self.pdat[sTTYFD])
+            tty.setraw(self.pdat.ttyfd)
             ch = sys.stdin.read(1)
             self.treset()
         finally:
@@ -180,25 +159,25 @@ class KBListener(threading.Thread):
         return ch
     
     def rstop(self):
-        self.pdat[sRT].reset_timer(0)
-        self.pdat[sRECORDFLAG] = False
-        self.pdat[sTHRESHOLD] = 100
-        self.pdat[sPROCESSOR].close()
+        self.pdat.rt.reset_timer(0)
+        self.pdat.recordflag = False
+        self.pdat.threshold = 100
+        self.pdat.processor.close()
 
     def run(self):
-        self.pdat[sTTYFD] = sys.stdin.fileno()
-        self.pdat[sTTYSETTINGS] = termios.tcgetattr(self.pdat[sTTYFD])
-        while self.pdat[sRUNNING]:
+        self.pdat.ttyfd = sys.stdin.fileno()
+        self.pdat.ttysettings = termios.tcgetattr(self.pdat.ttyfd)
+        while self.pdat.running:
             ch = self.getch()
             if ch == "h" or ch == "?":
                 print("h: help, f: show filename, k:show peak level, p: show peak")
                 print("q: quit, r: record on/off, v: set trigger level")
             elif ch == "k":
-                print("Peak/Trigger: " + str(self.pdat[sCURRENT]) + " " + str(self.pdat[sTHRESHOLD]))
+                print("Peak/Trigger: " + str(self.pdat.current) + " " + str(self.pdat.threshold))
             elif ch == "v":
                 self.treset()
-                pf = self.pdat[sPEAKFLAG]
-                self.pdat[sPEAKFLAG] = False
+                pf = self.pdat.peakflag
+                self.pdat.peakflag = False
                 try:
                     newpeak = int (input("New Peak Limit: "))
                 except:
@@ -206,28 +185,28 @@ class KBListener(threading.Thread):
                 if newpeak == 0:
                     print("? Number not recognized")
                 else:
-                    self.pdat[sTHRESHOLD] = newpeak
-                self.pdat[sPEAKFLAG] = pf
+                    self.pdat.threshold = newpeak
+                self.pdat.peakflag = pf
             elif ch == "f":
-                if self.pdat[sRECORDFLAG]:
-                    print("Filename: " + self.pdat[sPROCESSOR].filename)
+                if self.pdat.recordflag:
+                    print("Filename: " + self.pdat.processor.filename)
                 else:
                     print("Not recording")
             elif ch == "r":
-                if self.pdat[sRECORDFLAG]:
+                if self.pdat.recordflag:
                     self.rstop()
                     print("Recording disabled")
                 else:
-                    self.pdat[sRECORDFLAG] = True
-                    self.pdat[sTHRESHOLD] = 1
-                    self.pdat[sRT].reset_timer(time.time())
+                    self.pdat.recordflag = True
+                    self.pdat.threshold = 1
+                    self.pdat.rt.reset_timer(time.time())
                     print("Recording enabled")
             elif ch == "p":
-                self.pdat[sPEAKFLAG] = not self.pdat[sPEAKFLAG]
+                self.pdat.peakflag = not self.pdat.peakflag
             elif ch == "q":
                 print("Quitting...")
                 self.rstop()
-                self.pdat[sRUNNING] = False
+                self.pdat.running = False
                 self.treset()
                 time.sleep(0.5)
 #
@@ -241,45 +220,47 @@ parser.add_argument("-s", "--saverecs",  type=int, default=8,    help="Records t
 parser.add_argument("-t", "--threshold", type=int, default=99,   help="Minimum volume threshold (1-99) [99]")
 parser.add_argument("-l", "--hangdelay", type=int, default=6,    help="Seconds to record after input drops below threshold [6]")
 args = parser.parse_args()
-PDAT[sDEVINDEX] = args.devno
-PDAT[sTHRESHOLD] = args.threshold
-PDAT[sSAVERECS] = args.saverecs
-PDAT[sHANGDELAY] = args.hangdelay
-PDAT[sCHUNK] = args.chunk
+pdat = voxdat()
+
+pdat.devindex = args.devno
+pdat.threshold = args.threshold
+pdat.saverecs =  args.saverecs
+pdat.hangdelay = args.hangdelay
+pdat.chunk = args.chunk
 #
 # Fire up PyAudio and process the request
 #
 with noalsaerr():
-    PDAT[sPYAUDIO] = pyaudio.PyAudio()
+    pdat.pyaudio = pyaudio.PyAudio()
 
 if args.command == "listdevs":
     print("Device Information:")
-    for i in range(PDAT[sPYAUDIO].get_device_count()):
-        print("Dev#: ",i, PDAT[sPYAUDIO].get_device_info_by_index(i).get('name'))
+    for i in range(pdat.pyaudio.get_device_count()):
+        print("Dev#: ",i, pdat.pyaudio.get_device_info_by_index(i).get('name'))
 else:
-    PDAT[sSAMPLEQUEUE] = queue.Queue()
-    PDAT[sPREQUE] = queue.Queue()
+    pdat.samplequeue = queue.Queue()
+    pdat.preque = queue.Queue()
 
-    PDAT[sRUNNING] = True
-    PDAT[sRT] = _recordTimer(PDAT)
-    PDAT[sPROCESSOR] = _streamProcessor(PDAT)
-    PDAT[sPROCESSOR].start()
-    PDAT[sRT].start()
+    pdat.running = True
+    pdat.rt = _recordTimer(pdat)
+    pdat.processor = _streamProcessor(pdat)
+    pdat.processor.start()
+    pdat.rt.start()
 
-    PDAT[sDEVRATE] = int(PDAT[sPYAUDIO].get_device_info_by_index(PDAT[sDEVINDEX]).get('defaultSampleRate'))
-    PDAT[sDEVSTREAM] = PDAT[sPYAUDIO].open(format=FORMAT,
+    pdat.devrate = int(pdat.pyaudio.get_device_info_by_index(pdat.devindex).get('defaultSampleRate'))
+    pdat.devstream = pdat.pyaudio.open(format=FORMAT,
                                              channels=CHANNELS,
-                                             rate=PDAT[sDEVRATE],
+                                             rate=pdat.devrate,
                                              input=True,
-                                             input_device_index=PDAT[sDEVINDEX],
-                                             frames_per_buffer=PDAT[sCHUNK],
-                                             stream_callback=PDAT[sPROCESSOR].ReadCallback)
-    PDAT[sDEVSTREAM].start_stream()
+                                             input_device_index=pdat.devindex,
+                                             frames_per_buffer=pdat.chunk,
+                                             stream_callback=pdat.processor.ReadCallback)
+    pdat.devstream.start_stream()
 
-    PDAT[sKM] = KBListener(PDAT)
-    PDAT[sKM].start()
+    pdat.km = KBListener(pdat)
+    pdat.km.start()
 
-    while PDAT[sRUNNING]:
+    while pdat.running:
         time.sleep(1)
 
 print("Done.")
